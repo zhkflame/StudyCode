c8f04408cc4a7ac0d2bb1f01ac59e234e184cf7b
zhkflame <zhkflame@163.com>
2016-05-23 21:33:49 +0800

    update benford

--- leetcode/benford.py
+++ leetcode/benford.py
@@ -17,7 +17,7 @@ if __name__ =="__main__":
     n=1
     frequency=[0 for x in range(10)]
     print(frequency)
-    for i in range(1,1000,1):
+    for i in range(1,10000,1):
         if n>1000000000000:
             n=n//1000000000
         n=n*i
