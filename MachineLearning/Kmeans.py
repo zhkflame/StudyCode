dcb3283df6fd25e2e40d1b2601484307e0894c9d
zhkflame <zhkflame@163.com>
2016-06-01 07:42:08 +0800

    update Kmeans

--- MachineLearning/Kmeans.py
+++ MachineLearning/Kmeans.py
@@ -1,43 +1,97 @@
 from numpy import *
-import operator
-
-def k_means(data,k):
-    m=len(data)
-    n=len(data[0])
-    cluster=[-1 for x in range(m)]   #所有样本
-    cluster_center=[[]for x in range(k)]  #聚类的k个中心
-    c_next=[[] for x in range(k)]  #下一轮聚类中心
-    c_number=[0 for x in range(k)] #每个簇中样本的数目
-
-    #随机选取簇中心
-    i=0
-    while i<k:
-        j=random.randint(0,m-1)
-        if is_similar(data[j],cluster_center):
-            continue
-        cluster_center[i]=data[j][:]
-        c_next[i]=[0 for  x in range(n)]
-        i+=1
-    for times in range(40):
-        for i in range(m):
-            c=nearest(data[i],cluster_center)
-            cluster[i]=c
-            c_number[c]+=1
-            add(c_next[c],data[i])   #求第c簇的和
-        for i in range(k):
-            divide(c_next[i],c_number[i])  #求第i簇的均值
-            c_number[i]=0   #重新置0
-            cluster_center[i]=c_next[i][:]
-            zero_list(c_next[i])  #把c_next置0
-        print(times,cluster_center)
-    return cluster
+import time
+import matplotlib.pyplot as plt
 
 
-def is_similar(data,cluster_center):
-    return False
+# calculate Euclidean distance
+def euclDistance(vector1, vector2):
+    return sqrt(sum(power(vector2 - vector1, 2)))
 
-def nearest(data,cluster_center):
-    return
+# init centroids with random samples
+def initCentroids(dataSet, k):
+    numSamples, dim = dataSet.shape
+    centroids = zeros((k, dim))
+    for i in range(k):
+        index = int(random.uniform(0, numSamples))
+        centroids[i, :] = dataSet[index, :]
+    return centroids
 
-def zero_list(data):
-    return
+# k-means cluster
+def kmeans(dataSet, k):
+    numSamples = dataSet.shape[0]
+    # first column stores which cluster this sample belongs to,
+    # second column stores the error between this sample and its centroid
+    clusterAssment = mat(zeros((numSamples, 2)))
+    clusterChanged = True
+
+    ## step 1: init centroids
+    centroids = initCentroids(dataSet, k)
+
+    while clusterChanged:
+        clusterChanged = False
+        ## for each sample
+        for i in range(numSamples):
+            minDist  = 100000.0
+            minIndex = 0
+            ## for each centroid
+            ## step 2: find the centroid who is closest
+            for j in range(k):
+                distance = euclDistance(centroids[j, :], dataSet[i, :])
+                if distance < minDist:
+                    minDist  = distance
+                    minIndex = j
+
+            ## step 3: update its cluster
+            if clusterAssment[i, 0] != minIndex:
+                clusterChanged = True
+                clusterAssment[i, :] = minIndex, minDist**2
+
+        ## step 4: update centroids
+        for j in range(k):
+            pointsInCluster = dataSet[nonzero(clusterAssment[:, 0].A == j)[0]]
+            centroids[j, :] = mean(pointsInCluster, axis = 0)
+
+    print('Congratulations, cluster complete!')
+    return centroids, clusterAssment
+
+# show your cluster only available with 2-D data
+def showCluster(dataSet, k, centroids, clusterAssment):
+    numSamples, dim = dataSet.shape
+    if dim != 2:
+        print("Sorry! I can not draw because the dimension of your data is not 2!")
+        return 1
+
+    mark = ['or', 'ob', 'og', 'ok', '^r', '+r', 'sr', 'dr', '<r', 'pr']
+    if k > len(mark):
+        print("Sorry! Your k is too large! please contact Zouxy")
+        return 1
+
+    # draw all samples
+    for i in range(numSamples):
+        markIndex = int(clusterAssment[i, 0])
+        plt.plot(dataSet[i, 0], dataSet[i, 1], mark[markIndex])
+
+    mark = ['Dr', 'Db', 'Dg', 'Dk', '^b', '+b', 'sb', 'db', '<b', 'pb']
+    # draw the centroids
+    for i in range(k):
+        plt.plot(centroids[i, 0], centroids[i, 1], mark[i], markersize = 12)
+
+    plt.show()
+
+## step 1: load data
+print("step 1: load data...")
+dataSet = []
+fileIn = open('testKmeans.txt')
+for line in fileIn.readlines():
+    lineArr = line.strip().split()
+    dataSet.append([float(lineArr[0]), float(lineArr[1])])
+
+## step 2: clustering...
+print("step 2: clustering...")
+dataSet = mat(dataSet)
+k = 4
+centroids, clusterAssment = kmeans(dataSet, k)
+
+## step 3: show the result
+print("step 3: show the result...")
+showCluster(dataSet, k, centroids, clusterAssment)
